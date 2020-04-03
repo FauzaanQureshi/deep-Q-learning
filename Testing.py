9bbaae5bf8f0c02a5fb669e9928f52df2057dfcf
FauzaanQureshi <fauzaan11@gmail.com>
2020-04-04 03:24:45 +0530

    Test functions

--- Testing.py
+++ Testing.py
@@ -11,33 +11,45 @@ def gpu_test():
 
 
 def render_test():
-    env = gym.make("Breakout-v0")
+    env = gym.make("Pong-v0")
     env.reset()
     # print(env.action_space.sample())
-    for _ in range(1000):
+    r = 0
+    _ = 0
+    done = False
+    #for _ in range(1500):
+    while not done:
+        _ +=1
         env.render()
-        env.step(env.action_space.sample())
-        time.sleep(0.01)
+        _1, reward, done, _2 = env.step(env.action_space.sample())
+        r += reward
+        if _ % 100 == 0: print(r, done)
+        time.sleep(0.001)
+    print(_, r)
     env.close()
 
 
 def dqn_test():
-    env = gym.make("Breakout-v0")
+    env = gym.make("Pong-v0")
     env.reset()
     img = np.asanyarray(processed_screen(env.render("rgb_array"))).flatten()
-    print(img.shape)
-    dqn = DQN(img.shape[0])
+    #print(img.shape)
+    dqn_policy = DQN(img.shape[0])
 
-    for _ in range(1000):
+    dqn_target = DQN(img.shape[0])
+    dqn_target.model.set_weights(dqn_policy.model.get_weights())
+
+    for _ in range(0):
         env.render()
         screen = np.asanyarray(processed_screen(env.render("rgb_array"))).flatten()
-        action = dqn.forward(np.array([screen]))[0]
+        action = dqn_policy.forward(np.array([screen]))[0]
         if action[0] > action[1]:
             step = 2
         else:
             step = 5
         env.step(step)
         time.sleep(0.005)
+        if _ % 10 == 0: dqn_target.model.set_weights(dqn_policy.model.get_weights())
     env.close()
 
 
